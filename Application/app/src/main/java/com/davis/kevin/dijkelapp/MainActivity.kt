30f8980e02ead7789a54b30753d07d731078991b
Scazi <kevin-davis@hotmail.com>
2018-12-12 15:55:58 +0100

    Before fucking around

--- Application/app/src/main/java/com/davis/kevin/dijkelapp/MainActivity.kt
+++ Application/app/src/main/java/com/davis/kevin/dijkelapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.davis.kevin.dijkelapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToSettings(view: View){
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
