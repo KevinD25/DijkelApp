762ecce8225f1f6f2a1cd5c55a8e4c5256f31f56
Scazi <kevin-davis@hotmail.com>
2018-12-12 16:39:17 +0100

    fucking about worked

--- Application/app/src/main/java/com/davis/kevin/dijkelapp/SchachtenActivity.kt
+++ Application/app/src/main/java/com/davis/kevin/dijkelapp/SchachtenActivity.kt
@@ -2,11 +2,20 @@ package com.davis.kevin.dijkelapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
 
 class SchachtenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schachten)
+    }
+
+    fun openDialog(view:View){
+        MaterialDialog(this).show {
+            customView(R.layout.customdialogview)
+        }
     }
 }
