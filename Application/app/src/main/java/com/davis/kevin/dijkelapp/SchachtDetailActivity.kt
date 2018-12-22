1a49492906916b1e8196db81ab70f59d8673e743
Kevin Davis <kevin-davis@hotmail.com>
2018-12-23 00:02:01 +0100

    deleting and updating works

---
+++ Application/app/src/main/java/com/davis/kevin/dijkelapp/SchachtDetailActivity.kt
@@ -0,0 +1,123 @@
+package com.davis.kevin.dijkelapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.davis.kevin.dijkelapp.Adapters.DijkeltjesAdapter
+import com.davis.kevin.dijkelapp.DOM.Dijkel
+import com.davis.kevin.dijkelapp.DOM.Schacht
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_dijkel.*
+import kotlinx.android.synthetic.main.activity_schacht_detail.*
+
+class SchachtDetailActivity : AppCompatActivity() {
+
+    lateinit var item: Schacht
+    private var id: String = ""
+    lateinit var database: FirebaseDatabase
+    private lateinit var reference: DatabaseReference
+    private lateinit var dijkelref: DatabaseReference
+    private var schacht: Schacht? = null
+    private var schachtenLijst: MutableList<Schacht> = mutableListOf()
+    private var dijkel: Dijkel? = null
+    private var dijkelLijst: MutableList<Dijkel> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schacht_detail)
+        database = FirebaseDatabase.getInstance()
+        reference = FirebaseDatabase.getInstance().getReference("schachten")
+        dijkelref = FirebaseDatabase.getInstance().getReference("dijkels")
+        id = intent.getStringExtra("id")
+        fireBaseGet()
+    }
+
+    fun fillInfo() {
+        txtEditSchachtNaam.text = item.voornaam + " " + item.achternaam
+        editVoornaam.setText(item.voornaam)
+        editAchternaam.setText(item.achternaam)
+    }
+
+    fun getClickedSchacht() {
+        for (schacht in schachtenLijst) {
+            if (schacht.id == id) {
+                item = schacht
+            }
+        }
+        fillInfo()
+    }
+
+    fun onClickEdit(view:View){
+        val voornaam = editVoornaam.text.toString().trim()
+        val achternaam = editAchternaam.text.toString().trim()
+
+        if(voornaam.isEmpty())editVoornaam.error = "Empty field"
+        if(achternaam.isEmpty()) editAchternaam.error ="Empty field"
+
+        if(voornaam.isNotEmpty() && achternaam.isNotEmpty()){
+            val schacht = Schacht(item.id, voornaam, achternaam, item.dijkels)
+            reference.child(schacht.id).setValue(schacht)
+            super.onBackPressed()
+        }
+    }
+
+    fun onClickDelete(view:View){
+        reference.child(item.id).setValue(null)
+        for(dijkel in dijkelLijst) {
+            if(dijkelref.child(dijkel.id).child("schachtid").toString() == item.id){
+                dijkelref.child(dijkel.id).setValue(null)
+            }
+        }
+        super.onBackPressed()
+    }
+
+    fun fireBaseGet() {
+        val schachtListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                // Get Post object and use the values to update the UI
+                if (dataSnapshot.exists()) {
+                    schachtenLijst.clear()
+                    for (h in dataSnapshot.children) {
+                        schacht = h.getValue(Schacht::class.java)
+                        schachtenLijst.add(schacht!!)
+                    }
+                    getClickedSchacht()
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("TAG", "loadPost:onCancelled", databaseError.toException())
+                // ...
+            }
+        }
+        reference.addValueEventListener(schachtListener)
+
+        val dijkelListener = object : ValueEventListener {
+            override fun onDataChange(dijkelSnapshot: DataSnapshot) {
+
+                // Get Post object and use the values to update the UI
+                if (dijkelSnapshot.exists()) {
+                    dijkelLijst.clear()
+
+                    for (h in dijkelSnapshot.children) {
+                        dijkel = h.getValue(Dijkel::class.java)
+                        dijkelLijst.add(dijkel!!)
+                    }
+
+                    val adapter = DijkeltjesAdapter(applicationContext, dijkelLijst)
+                    listDijkels.adapter = adapter
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("TAG", "loadPost:onCancelled", databaseError.toException())
+                // ...
+            }
+        }
+        dijkelref.addValueEventListener(dijkelListener)
+    }
+}
--- Application/app/src/main/java/com/davis/kevin/dijkelapp/SchachtenActivity.kt
+++ Application/app/src/main/java/com/davis/kevin/dijkelapp/SchachtenActivity.kt
@@ -1,9 +1,11 @@
 package com.davis.kevin.dijkelapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -13,6 +15,7 @@ import com.afollestad.materialdialogs.customview.getCustomView
 import com.davis.kevin.dijkelapp.Adapters.SchachtenLijstAdapter
 import com.davis.kevin.dijkelapp.DOM.Schacht
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_schachten.*
 
 class SchachtenActivity : AppCompatActivity() {
@@ -22,7 +25,7 @@ class SchachtenActivity : AppCompatActivity() {
     private lateinit var reference: DatabaseReference
     private var schacht: Schacht? = null
     private var schachtenLijst: MutableList<Schacht> = mutableListOf()
-
+    lateinit var item : Schacht
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,22 +33,22 @@ class SchachtenActivity : AppCompatActivity() {
         database = FirebaseDatabase.getInstance()
         reference = FirebaseDatabase.getInstance().getReference("schachten")
 
-
-       // val emptylist = Schacht("1337", "Nog geen", "schachten toegevoegd...", 0)
-        //schachtenLijst.add(emptylist)
-
         fireBaseGet()
 
-
+        listSchachtenAdmin.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            // This is your listview's selected item
+            item = parent.getItemAtPosition(position) as Schacht
+            openEditActivity()
+        }
     }
 
     fun fireBaseGet() {
         val schachtListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
-               /* if (schachtenLijst[0].id == "1337") {
-                    schachtenLijst.clear()
-                }*/
+                /* if (schachtenLijst[0].id == "1337") {
+                     schachtenLijst.clear()
+                 }*/
                 if (dataSnapshot.exists()) {
                     schachtenLijst.clear()
                     for (h in dataSnapshot.children) {
@@ -65,7 +68,6 @@ class SchachtenActivity : AppCompatActivity() {
         }
         reference.addValueEventListener(schachtListener)
     }
-
 
     fun openDialog(view: View) {
         val dialog = MaterialDialog(this).show {
@@ -87,6 +89,37 @@ class SchachtenActivity : AppCompatActivity() {
             dialog.dismiss()
         }
 
+    }
+
+    fun openDeleteDialog(view: View) {
+        val dialog = MaterialDialog(this).show {
+            customView(R.layout.deletealldialog)
+        }
+
+        // Setup custom view content
+        customView = dialog.getCustomView() ?: return
+        val buttonDeleteAll: Button = customView.findViewById(R.id.btnDeleteAll)
+        val buttonCancel: Button = customView.findViewById(R.id.btnCancel)
+
+        buttonDeleteAll.setOnClickListener {
+            onClickDeleteAll()
+            dialog.dismiss()
+        }
+        buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+
+    fun openEditActivity(){
+        val intent = Intent(this, SchachtDetailActivity::class.java)
+        intent.putExtra("id", item.id)
+        startActivity(intent)
+    }
+
+    fun onClickDeleteAll() {
+        for(item in schachtenLijst){
+            reference.child(item.id).setValue(null)
+        }
     }
 
     fun onClickConfirm(voornaamInput: EditText, achternaamInput: EditText) {
@@ -113,3 +146,4 @@ class SchachtenActivity : AppCompatActivity() {
         return voornaam != "" && achternaam != ""
     }
 }
+
